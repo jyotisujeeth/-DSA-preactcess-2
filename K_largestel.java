d31702fe47478c8b940def3801d1917a69f6a3f9
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-14 22:48:20 +0530

    Update K_largestel.java

--- K_largestel.java
+++ K_largestel.java
@@ -1,1 +1,15 @@
-
+/*
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+*/
+//Sort
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        // Can't sort int[] in descending order in Java;
+        // Sort ascending and then return the kth element from the end
+        return nums[nums.length - k];
+    }
+}
+/////////////////////////////////////////////////////
+//Min-Heap
