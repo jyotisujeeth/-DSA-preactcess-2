4f038c5a314e28b04165275cff6f6b969d7d5951
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-16 13:51:10 +0530

    Update slidingWindow.js

--- slidingWindow.js
+++ slidingWindow.js
@@ -1,7 +1,15 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
+/*
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
  */
 var maxSlidingWindow = function(arr, w) {
 
