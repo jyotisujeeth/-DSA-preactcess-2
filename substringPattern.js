f7c57f72eecd6e010fff1ddce7f32ba2235e7095
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-21 19:03:56 +0530

    Update substringPattern.js

--- substringPattern.js
+++ substringPattern.js
@@ -1,1 +1,18 @@
-
+/*
+Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+*/
+var repeatedSubstringPattern = function(s) {
+    return s.repeat(2).slice(1, -1).includes(s);
+};
+//////////////////////////////////////////////////////////////
+var repeatedSubstringPattern = function(s) {
+    let i = 1, len = s.length;
+    while (i <= Math.floor(len/2)) {
+        if (s.slice(0, i).repeat(len/i) == s) return true;
+        i++
+    }
+    return false;
+};
+/////////////////////////////////////////////////////////////////////////
