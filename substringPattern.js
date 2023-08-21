dd1d4b80de346285ff4227ed85db66b772e4936e
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-21 19:04:08 +0530

    Update substringPattern.js

--- substringPattern.js
+++ substringPattern.js
@@ -16,3 +16,11 @@ var repeatedSubstringPattern = function(s) {
     return false;
 };
 /////////////////////////////////////////////////////////////////////////
+var repeatedSubstringPattern = function(s) {
+    let pattern = "";
+    for (let i = 0; i < Math.floor(s.length/2); i++) {
+        pattern += s[i];
+        if (pattern.repeat(s.length / pattern.length) === s) return true;
+    }
+    return false;
+};
