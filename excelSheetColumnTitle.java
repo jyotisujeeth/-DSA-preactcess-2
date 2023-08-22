c390505eafd89ad69a57cc87fe498ebb37e8ff20
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-22 18:47:05 +0530

    Update excelSheetColumnTitle.java

--- excelSheetColumnTitle.java
+++ excelSheetColumnTitle.java
@@ -1,1 +1,28 @@
-
+/*
+Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+Input: columnNumber = 1
+Output: "A"
+  */
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder ans = new StringBuilder();
+        
+        while(columnNumber > 0) {
+            columnNumber--;
+            // Get the last character and append it at the end of the string.
+            ans.append((char)((columnNumber) % 26 + 'A'));
+            columnNumber = (columnNumber) / 26;
+        }
+        
+        // Reverse it, as we appended characters in reverse order.
+        return ans.reverse().toString();
+    }
+}
