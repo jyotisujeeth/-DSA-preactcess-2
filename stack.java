d02700da4964551d6bdd2a5b9e47852e03dca9f1
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-08-28 12:41:34 +0530

    Update stack.java

--- stack.java
+++ stack.java
@@ -1,4 +1,41 @@
 // Implement Stack using Queues
+////////////////////////////////////////////Push///////////////////////////
+private Queue<Integer> q1 = new LinkedList<>();
+private Queue<Integer> q2 = new LinkedList<>();
+private int top;
+
+// Push element x onto stack.
+public void push(int x) {
+    q1.add(x);
+    top = x;
+}
+////////////////////////////////////////////////////////////////////////////////////////////
+public void push(int x) {
+    q2.add(x);
+    top = x;
+    while (!q1.isEmpty()) {                
+        q2.add(q1.remove());
+    }
+    Queue<Integer> temp = q1;
+    q1 = q2;
+    q2 = temp;
+}
+////////////////////////////////////////////////pop/////////////////////////////////////////
+// Removes the element on top of the stack.
+public void pop() {
+    while (q1.size() > 1) {
+        top = q1.remove();
+        q2.add(top);
+    }
+    q1.remove();
+    Queue<Integer> temp = q1;
+    q1 = q2;
+    q2 = temp;
+}
+//////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+
 class MyStack {
 	
 	private Queue<Integer> queue = new LinkedList<>();
