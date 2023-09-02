fd8a2d7b5361a7289ba4628b319ba2436da75247
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-09-02 19:14:20 +0530

    Update characters.java

--- characters.java
+++ characters.java
@@ -1,4 +1,9 @@
-class Solution {
+/*
+Extra Characters in a String
+Input: s = "leetscode", dictionary = ["leet","code","leetcode"]
+Output: 1
+Explanation: We can break s in two substrings: "leet" from index 0 to 3 and "code" from index 5 to 8. There is only 1 unused character (at index 4), so we return 1.
+*/class Solution {
     Integer[] memo;
     HashSet<String> dictionarySet;
     public int minExtraChar(String s, String[] dictionary) {
